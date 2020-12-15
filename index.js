949293c521a74896c836795f3516dcadc965d04b
Inosaka Manami <inosakamanami@gmail.com>
2020-12-15 21:05:52 +0900

    変更

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function _onSubmit(e) {
     const query = e.target.elements['q'].value;
-    if(!query) {
+    if (!query) {
         window.alert('本のタイトルを入力してください。');
         e.preventDefault(); // formタグのSubmitを止める
     }
